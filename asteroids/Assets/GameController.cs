eed309f0370cf8928288729b024ef5a39b1c9a57
Vitor Shade <vitor.shade@gmail.com>
2014-02-09 16:56:28 -0200

    -Created game controller script -Added player live and score -Moved player ship rendering code to a separate script, so that the same code can be easily used to draw the lives icons

---
+++ asteroids/Assets/GameController.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+enum GAME_STATE
+{
+    MAIN_MENU,
+    PLAYING,
+    GAME_OVER,
+    HIGH_SCORES
+}
+;
+
+public class GameController : MonoBehaviour
+{
+    public PlayerShip player_ship_;
+    public GUIText score_text_;
+    public GameObject hud_player_life_;
+    public float hud_life_distance_;
+
+    private GAME_STATE curr_state_;
+    private int current_score_;
+    private int num_lives_;
+    private bool num_lives_changed_;
+    private List<GameObject> hud_player_lives_;
+
+    // Use this for initialization
+    void Start()
+    {
+        num_lives_ = 3;
+        hud_player_lives_ = new List<GameObject>();
+        for (int i = 0; i < num_lives_; i++)
+        {
+            AddLife();
+        }
+        curr_state_ = GAME_STATE.PLAYING;
+        GameObject.Instantiate(player_ship_);        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (curr_state_)
+        {
+            case GAME_STATE.MAIN_MENU:
+                break;
+            case GAME_STATE.PLAYING:
+                break;
+            case GAME_STATE.HIGH_SCORES:
+                break;
+            case GAME_STATE.GAME_OVER:
+                break;
+        }
+    }
+
+    void AddScore(int score)
+    {
+        current_score_ += score;
+        score_text_.text = current_score_.ToString();
+        if (current_score_ % 10.000 == 0)
+        {
+            AddLife();
+        }
+    }
+
+    void AddLife()
+    {
+        float shift = hud_player_lives_.Count * hud_life_distance_;
+        GameObject hud_life = (GameObject)GameObject.Instantiate(hud_player_life_);        
+        Vector3 position = Camera.main.ViewportToWorldPoint(new Vector3(0.08f + shift, 0.9f, 0.0f));
+        position.z = 0.0f;
+        hud_life.transform.position = position;
+        hud_player_lives_.Add(hud_life);
+    }
+
+    void OnPlayerDeath()
+    {
+        Destroy(hud_player_lives_[num_lives_ - 1]);
+        hud_player_lives_.RemoveAt(num_lives_ - 1);
+        num_lives_ -= 1;
+    }
+}
--- asteroids/Assets/PlayerShip.cs
+++ asteroids/Assets/PlayerShip.cs
@@ -7,33 +7,17 @@ public class PlayerShip : MonoBehaviour
 		public float thrust_force_;
 		public float rotate_speed_;
 		public float max_velocity_;
-		public Rigidbody2D rigid_body_;
-		private Material line_material_;
-		private List<Vector3> vertices_;
+		public Rigidbody2D rigid_body_;		
 		// Use this for initialization
 		void Start ()
-		{
-				CreateLineMaterial ();
-				Vector3 p1 = new Vector3 (-0.5f, -0.5f, 0.0f);
-				Vector3 p2 = new Vector3 (0.0f, 0.5f, 0.0f);
-				Vector3 p3 = new Vector3 (0.5f, -0.5f, 0.0f);
-				Vector3 p4 = new Vector3 (0.25f, -0.25f, 0.0f);
-				Vector3 p5 = new Vector3 (-0.25f, -0.25f, 0.0f);
-				vertices_ = new List<Vector3> ();
-				vertices_.Add (p1);
-				vertices_.Add (p2);
-				vertices_.Add (p3);
-				vertices_.Add (p4);
-				vertices_.Add (p5);
+		{				
 		}
 	
 		// Update is called once per frame
 		void Update ()
 		{
 				transform.Rotate (Vector3.forward * ((-1) * Input.GetAxis ("Horizontal") * rotate_speed_ * Time.deltaTime));
-				
-
-		}
+        }
 	
 		void FixedUpdate ()
 		{					
@@ -45,66 +29,6 @@ public class PlayerShip : MonoBehaviour
 						Vector2 curr_velocity = rigid_body_.velocity;
 						rigid_body_.velocity = curr_velocity.normalized * max_velocity_;
 				}	
-		}
-    
-		void CreateLineMaterial ()
-		{
-				if (!line_material_) {
-						line_material_ = new Material ("Shader \"Lines/Colored Blended\" {" +
-								"SubShader { Pass { " +
-								"    Blend SrcAlpha OneMinusSrcAlpha " +
-								"    ZWrite Off Cull Off Fog { Mode Off } " +
-								"    BindChannels {" +
-								"      Bind \"vertex\", vertex Bind \"color\", color }" +
-								"} } }");
-						line_material_.hideFlags = HideFlags.HideAndDontSave;
-						line_material_.shader.hideFlags = HideFlags.HideAndDontSave;
-				}
-		}
-	
-		void OnGUI ()
-		{
-				RenderShip ();
-		}
-		
-		void RenderShip ()
-		{
-				line_material_.SetPass (0);
-				GL.PushMatrix ();		
-				Matrix4x4 trs_matrix = Matrix4x4.TRS (transform.position, transform.rotation, Vector3.one);
-				GL.MultMatrix (trs_matrix);		
-				DrawLines ();
-				DrawSquares (0.02f);
-				GL.PopMatrix ();
-		}
-    
-		void DrawLines ()
-		{
-				GL.Color (new Color (1.0f, 1.0f, 1.0f, 0.5f));
-				GL.Begin (GL.LINES);
-				for (int i = 0; i < vertices_.Count; i++) {
-						GL.Vertex (vertices_ [i]);
-						if (i < vertices_.Count - 1) {
-								GL.Vertex (vertices_ [i + 1]);
-						}				
-				}
-				GL.Vertex (vertices_ [0]);
-				GL.End ();
-		}
-		
-		void DrawSquares (float size)
-		{
-				float half_size = size / 2.0f;
-				GL.Color (new Color (1.0f, 1.0f, 1.0f, 1.0f));
-				GL.Begin (GL.QUADS);
-				for (int i = 0; i < vertices_.Count; i++) {
-						Vector3 vertice = vertices_ [i];
-						GL.Vertex3 (vertice.x - half_size, vertice.y - half_size, 0.0f);
-						GL.Vertex3 (vertice.x + half_size, vertice.y - half_size, 0.0f);
-						GL.Vertex3 (vertice.x + half_size, vertice.y + half_size, 0.0f);
-						GL.Vertex3 (vertice.x - half_size, vertice.y + half_size, 0.0f);
-				}				
-				GL.End ();
 		}
     
 }
